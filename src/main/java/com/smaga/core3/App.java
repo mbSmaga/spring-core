7f16efcde97e036df0fedee62339a58941748b31
mbSmaga <mb.smaga@gmail.com>
2019-10-26 07:00:37 +0200

    task 3 profiles

--- src/main/java/com/smaga/core3/App.java
+++ src/main/java/com/smaga/core3/App.java
@@ -2,12 +2,16 @@ package com.smaga.core3;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
         Person person = (Person) context.getBean("person");
@@ -15,5 +19,10 @@ public class App {
 
         AddressBook addressBook = (AddressBook) context.getBean("addressBook");
         System.out.println(addressBook);
+
+        Environment environment = context.getEnvironment();
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+        DatabaseConnector connector = (DatabaseConnector) context.getBean("databaseConnector");
+        System.out.println(connector.databaseUrl);
     }
 }
---
+++ src/main/java/com/smaga/core3/DatabaseConnector.java
@@ -0,0 +1,9 @@
+package com.smaga.core3;
+
+public class DatabaseConnector {
+    String databaseUrl;
+
+    public DatabaseConnector(String databaseUrl) {
+        this.databaseUrl = databaseUrl;
+    }
+}
